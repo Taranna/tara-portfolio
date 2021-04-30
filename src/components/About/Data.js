49d0769c2d075f2ef1cf314ca9b6c40d4f0a34f5
Tara Mathews <52750864+Taranna@users.noreply.github.com>
2021-04-30 16:31:59 +0530

    Update Data.js

--- src/components/About/Data.js
+++ src/components/About/Data.js
@@ -1,50 +1,16 @@
-// export const homeObjOne = {
-//   id: "about",
-//   lightBg: false,
-//   lightText: true,
-//   lightTextDesc: true,
-//   topLine: "About",
-//   headLine: "I am interested in a career as a Full Stack Developer ",
-//   description: "I am interested in a career as a Full Stack Developer ",
-//   buttonLabel: "FrontEnd",
-//   imgStart: false,
-//   img: require("../../images/imgg.png"),
-//   alt: "img6",
-//   dark: true,
-//   primary: true,
-//   darkText: false,
-// };
 
-// export const homeObjTwo = {
-//   id: "projects",
-//   lightBg: true,
-//   lightText: false,
-//   lightTextDesc: false,
-//   topLine: "Project",
-//   headLine: "Projects",
-//   description: "These are my projects",
-//   buttonLabel: "Github",
-
-//   imgStart: true,
-//   img: require("../../images/per.svg"),
-//   alt: "img6",
-//   dark: false,
-//   primary: false,
-//   darkText: true,
-// };
 export const homeObjTwo = {
   id: "about",
   lightBg: true,
   lightText: false,
   lightTextDesc: false,
-  // topLine: "About ",
+ 
   headLine: "About",
-  //   description: " ",
+
   buttonLabel: "Github",
   imgStart: false,
 
-  // img: require("../../images/per.svg"),
-  //alt: "img6",
+  ,
   dark: false,
   primary: false,
   darkText: true,
